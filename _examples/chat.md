3a2aa0f60d97c367f52f92ff340818835c07f435
Olivier Peltre <opeltre@gmail.com>
2020-05-01 10:46:04 +0200

    track chat

---
+++ _examples/chat.md
@@ -0,0 +1,68 @@
+---
+title:  Chat Room 
+script: chatroom
+name:   chat room
+app:    false
+---
+<div class="flex-h"> 
+{% include_relative includes/app_div.html id="bob" %}  
+{% include_relative includes/app_div.html id="alice" %}  
+</div> 
+
+IO streams effectively channel interactions between isolated components. 
+
+
+
+<style> 
+.msgs {
+    overflow-y: auto;
+    height: 250px;
+}
+
+.chat {
+    padding: 10px;
+    min-width: 80%;
+}
+
+.input {
+    display: flex;
+    flex-flow: row;
+}
+.input textarea {
+    font-family: "iosevka light";
+    font-size: 14px;
+    flex-grow: 1;
+    color:  #555;
+}
+.msg-sent, .msg-received {
+    padding-bottom: 15px;
+}
+.msg-body {
+    padding: 6px; 
+    margin: 2px;
+    font-size: 16px;
+    border: 1px solid #dde;
+    border-radius: 6px;
+}
+.msg-body.sent {
+    margin-right: 0;
+    margin-left: auto;
+}
+.msg-body.received {
+    margin-left: 0;
+    margin-right: auto;
+}
+
+.msg-head {
+    display: flex;
+    flex-flow: row;
+    justify-content: space-between;
+    font-size: 10px;
+}
+.msg-head .date {
+    color: #bbc;
+}
+.msg-body {
+    max-width: 60%;
+}
+</style> 
---
+++ _examples/source/chatroom.js
@@ -0,0 +1,130 @@
+let _r = __.record(),
+    IO = dom.IO;
+
+//  M : Model 
+let M = {
+    user:   'alice',
+    msgs:   [],
+    input:  ''
+}
+
+//  read : Model -> [Msg] 
+let read = M => M.msgs
+    .map(_r.set('user', M.user));
+
+//  send : Model -> Msg
+let send = M => ({
+    sender: M.user,
+    body:   M.input,
+    date:   Date.now()
+});
+
+
+//------ Server ------ 
+
+let server = {};
+
+//    .msgs : [Msg] 
+server.msgs = [{
+    sender: 'alice',
+    date:   0,
+    body: 'I love you Bob'
+}];
+
+//    .clients : [IO(e)] 
+server.clients = [];
+
+//    .post : Msg -> ServerEffect
+server.post = msg => {
+    server.msgs.push(msg);
+    server.clients.forEach(
+        io => io.channel('msg', msg)
+    );
+}
+
+//    .join : User -> ServerEffect
+server.join = user => server.clients.push(
+    client(user, server)
+);
+
+
+//------ Client ------
+
+//  client : (User, Server) -> IO(e) 
+let client = (user, server) => 
+    client.app.start({
+        user,
+        msgs:   server.msgs,
+        input:  ''
+    });
+
+//    .app : e -> State(Model, IO(e))
+client.app = dom.app()
+    .on('start', st => st
+        .reads(IO.put(view.main))
+    )
+    .on('input', (st, text) => st
+        .set('input', text)
+        .return(IO())
+    )
+    .on('msg', (st, msg) => st
+        .streamline({msgs: M => [...M.msgs, msg]})
+        .reads(read) 
+        .push(IO.map(view.msg).put([-1]))
+    )
+    .on('send', st => st
+        .set('input', '')
+        .reads(IO.replace(view.input))
+    )
+    .hook('send', st => st
+        .reads(send)
+        .push(msg => () => server.post(msg))
+    );
+
+
+//------ View ------ 
+
+let view = {};
+
+//  .msg : Msg -> Node
+view.msg = dom(':msg > msgs', [
+    ['.msg-head', [
+        ['.sender', {
+            html: m => m.sender === m.user ? 'you say:' : m.sender + ' says:'
+        }],
+        ['.date', {html: m => `on 1 Jan. 1970 and ${m.date} ms`}]
+    ]],
+    ['.msg-body', {
+        html: m => m.body,
+        class: m => m.sender === m.user ? 'sent' : 'received'
+    }]
+]);
+
+//  .msgs : Model -> [Node]
+view.msgs = dom(':msgs')
+    .pull(read)
+    .branch(dom.map(view.msg))
+
+//  .input : Model -> Node
+view.input = dom(':input', [
+    ['textarea', {
+        onkeyup: (e, io) => io.channel('input', e.target.value)
+    }],
+    ['button', {
+        html: 'Send',
+        onclick: (e, io) => io.channel('send')
+    }]
+]);
+
+//  .main : Model -> Node 
+view.main = dom('.chat')
+    .append(
+        view.msgs,
+        view.input
+    )
+    .put(M => '#' + M.user);
+
+
+//------ Start a Romance ------
+
+['alice', 'bob'].forEach(server.join); 
